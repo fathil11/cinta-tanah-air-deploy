b84403cd6851a7d30978778923e28ed3348af5d5
fathil11 <fathil.arham@gmail.com>
2019-08-13 07:45:13 +0700

    Profil page fix

--- cinta-tanah-air/resources/views/home/profil.blade.php
+++ cinta-tanah-air/resources/views/home/profil.blade.php
@@ -30,21 +30,22 @@ Profil Kami
                         <div class="media contact-info">
                             <span class="contact-info__icon"><i class="ti-home"></i></span>
                             <div class="media-body">
-                                <h3>Semarang Tengah, Semarang.</h3>
-                                <p>Jl. Pemuda No.15</p>
+                                <h3>Semarang .</h3>
+                                <p>Pesantren Jati Diri Bangsa Indonesia Merajut Perdamaian Nusantara, Kel Gunung Pati,
+                                    Kec Gunung Pati, Kota Semarang, Jawa Tengah.</p>
                             </div>
                         </div>
                         <div class="media contact-info">
                             <span class="contact-info__icon"><i class="ti-tablet"></i></span>
                             <div class="media-body">
-                                <h3>(024) 547 098</h3>
-                                <p>10 Pagi - 5 Sore</p>
+                                <h3>0812-2870-3110</h3>
+                                <p>8 Pagi - 5 Sore</p>
                             </div>
                         </div>
                         <div class="media contact-info">
                             <span class="contact-info__icon"><i class="ti-email"></i></span>
                             <div class="media-body">
-                                <h3>cintatanahair@gmail.com</h3>
+                                <h3>info@cintatanahair.id</h3>
                                 <p>Beri kami masukan .</p>
                             </div>
                         </div>
