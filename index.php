6253f016012d65dd808944c5b651777e0d73cd19
fathil11 <fathil.arham@gmail.com>
2019-08-13 17:16:18 +0700

    testing 5

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/cinta-tanah-air/vendor/autoload.php';
+require __DIR__.'/../cinta-tanah-air/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/cinta-tanah-air/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/cinta-tanah-air/bootstrap/app.php';
+$app = require_once __DIR__.'/../cinta-tanah-air/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
---
+++ symlinkcreate.php
@@ -0,0 +1,2 @@
+<?php
+symlink('/home/u7186425/public_html', '/home/u7186425/public_html');
