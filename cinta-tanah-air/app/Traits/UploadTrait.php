b2779c75c8d1e130772d4866ff8eb784a70e0627
fathil11 <fathil.arham@gmail.com>
2019-08-13 17:56:49 +0700

    tes 3

--- cinta-tanah-air/app/Traits/UploadTrait.php
+++ cinta-tanah-air/app/Traits/UploadTrait.php
@@ -9,7 +9,7 @@ trait UploadTrait
 {
     public function uploadOne(UploadedFile $uploadedFile, $folder = null, $disk = 'public', $filename = null)
     {
-        dd($folder);
+        dd($uploadedFile);
         $name = !is_null($filename) ? $filename : str_random(25);
 
         $file = $uploadedFile->storeAs($folder, $name . '.' . $uploadedFile->getClientOriginalExtension(), $disk);
