36a9fbcd395d6d5a380f89e08dd8c05103b73399
fathil11 <fathil.arham@gmail.com>
2019-08-13 18:25:01 +0700

    kk

--- cinta-tanah-air/app/Traits/UploadTrait.php
+++ cinta-tanah-air/app/Traits/UploadTrait.php
@@ -10,7 +10,7 @@ trait UploadTrait
     public function uploadOne(UploadedFile $uploadedFile, $folder = null, $disk = 'public', $filename = null)
     {
         $folder = 'www.cintatanahair.id'. $folder;
-        dd($folder);
+        // dd($folder);
         $name = !is_null($filename) ? $filename : str_random(25);
 
         $file = $uploadedFile->storeAs($folder, $name . '.' . $uploadedFile->getClientOriginalExtension(), $disk);
