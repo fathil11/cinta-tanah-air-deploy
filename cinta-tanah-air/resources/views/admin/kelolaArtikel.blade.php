6fc584a3af941cdbcfcdc3db03db896f18bb9ec5
fathil11 <fathil.arham@gmail.com>
2019-08-15 13:39:03 +0700

    Fix Delete Message

--- cinta-tanah-air/resources/views/admin/kelolaArtikel.blade.php
+++ cinta-tanah-air/resources/views/admin/kelolaArtikel.blade.php
@@ -24,7 +24,7 @@
         @if (Session::has('error'))
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
             <span class="alert-inner--icon"><i class="ni ni-bell-55"></i></span>
-            <span class="alert-inner--text"> {{ Session::get('status') }}</span>
+            <span class="alert-inner--text"> {{ Session::get('error') }}</span>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
