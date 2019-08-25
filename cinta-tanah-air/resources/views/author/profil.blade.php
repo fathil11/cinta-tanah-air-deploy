daac2dfb9e01b57d7dbf9602dc9f6aa04f6ea252
fathil11 <fathil.arham@gmail.com>
2019-08-26 04:11:14 +0700

    Final Fixing Message Layout in Author Profile

--- cinta-tanah-air/resources/views/author/profil.blade.php
+++ cinta-tanah-air/resources/views/author/profil.blade.php
@@ -20,7 +20,7 @@
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
-        @elseif (Session::has('error'))
+        @elseif ($errors->any())
         <div class="col-md-12">
             <div class="alert alert-warning alert-dismissible fade show" role="alert">
                 <div>
