91f416098fdd4b55dfdad98874b321bace271663
fathil11 <fathil.arham@gmail.com>
2019-08-26 04:27:10 +0700

    Fix

--- cinta-tanah-air/resources/views/layouts/validationMessage.blade.php
+++ cinta-tanah-air/resources/views/layouts/validationMessage.blade.php
@@ -18,7 +18,7 @@
 <div class="alert alert-warning alert-dismissible fade show" role="alert">
     <div>
         @foreach ($errors->all() as $error)
-        <li>
+        <li style="list-style-type: none;">
             <span class="alert-inner--icon"><i class="ni ni-bell-55"></i></span>
             <span class="alert-inner--text">{{ $error }}</span>
         </li>
