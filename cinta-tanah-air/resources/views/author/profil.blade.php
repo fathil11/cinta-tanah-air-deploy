1f65e36caa5aa83da3d3b6b52356d9ae92f89bbc
fathil11 <fathil.arham@gmail.com>
2019-08-26 04:15:39 +0700

    Refactoring Validation Message

--- cinta-tanah-air/resources/views/author/profil.blade.php
+++ cinta-tanah-air/resources/views/author/profil.blade.php
@@ -12,30 +12,7 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
-        @if (Session::has('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <span class="alert-inner--icon"><i class="ni ni-bell-55"></i></span>
-            <span class="alert-inner--text"> {{ Session::get('success') }}</span>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        @elseif ($errors->any())
-        <div class="col-md-12">
-            <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                <div>
-                    @foreach ($errors->all() as $error)
-                    <li>
-                        <span class="alert-inner--text">{{ $error }}</span>
-                    </li>
-                    @endforeach
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            </div>
-        </div>
-        @endif
+        @include('layouts.validationMessage')
     </div>
     <div class="col">
         <div class="card shadow cardcustom">
---
+++ cinta-tanah-air/resources/views/layouts/validationMessage.blade.php
@@ -0,0 +1,32 @@
+@if (Session::has('success'))
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <span class="alert-inner--icon"><i class="ni ni-bell-55"></i></span>
+    <span class="alert-inner--text"> {{ Session::get('success') }}</span>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@elseif (Session::has('error'))
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <span class="alert-inner--icon"><i class="ni ni-bell-55"></i></span>
+    <span class="alert-inner--text"> {{ Session::get('error') }}</span>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@elseif ($errors->any())
+<div class="col-md-12">
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <div>
+            @foreach ($errors->all() as $error)
+            <li>
+                <span class="alert-inner--text">{{ $error }}</span>
+            </li>
+            @endforeach
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    </div>
+</div>
+@endif
