c69aa3b97172c1ad2ddac5ecc72976ddba539ae8
fathil11 <fathil.arham@gmail.com>
2019-08-26 04:18:49 +0700

    Fixing Validation Message Layout

--- cinta-tanah-air/resources/views/layouts/validationMessage.blade.php
+++ cinta-tanah-air/resources/views/layouts/validationMessage.blade.php
@@ -15,18 +15,16 @@
     </button>
 </div>
 @elseif ($errors->any())
-<div class="col-md-12">
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <div>
-            @foreach ($errors->all() as $error)
-            <li>
-                <span class="alert-inner--text">{{ $error }}</span>
-            </li>
-            @endforeach
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <div>
+        @foreach ($errors->all() as $error)
+        <li>
+            <span class="alert-inner--text">{{ $error }}</span>
+        </li>
+        @endforeach
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 </div>
 @endif
