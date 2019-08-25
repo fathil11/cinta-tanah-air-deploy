6ce352cd1aac208dcb48e0776e4a1f62257122ae
fathil11 <fathil.arham@gmail.com>
2019-08-26 01:46:35 +0700

    Test Hidin

--- cinta-tanah-air/resources/views/author/editArtikel.blade.php
+++ cinta-tanah-air/resources/views/author/editArtikel.blade.php
@@ -32,7 +32,7 @@
             <form action="{{ url('author/edit-artikel') . '/' . $article->id }}" method="post"
                 enctype="multipart/form-data">
                 {{ csrf_field() }}
-                <input class="d-none" type="hidden" name="article_id" value="{{ $article->id }}">
+                <input type="hidden" name="article_id" value="{{ $article->id }}" hidden>
 
                 <div class="container-fluid">
                     <br>
