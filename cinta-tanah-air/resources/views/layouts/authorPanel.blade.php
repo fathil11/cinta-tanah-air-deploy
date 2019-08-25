c7ff6e525ea59bbeffb485bf6c516757a3fcf022
fathil11 <fathil.arham@gmail.com>
2019-08-26 04:33:33 +0700

    Add Active Condition on Profil Menu

--- cinta-tanah-air/resources/views/layouts/authorPanel.blade.php
+++ cinta-tanah-air/resources/views/layouts/authorPanel.blade.php
@@ -126,7 +126,8 @@
 
                 <ul class="navbar-nav mb-md-3">
                     <li class="nav-item">
-                        <a class="nav-link  active " href="{{ url('author/profil') }}">
+                        <a class="nav-link @if(Request::is('author/kelola-artikel')) {{'active'}} @endif"
+                            href="{{ url('author/profil') }}">
                             <i class="ni ni-single-02 text-yellow"></i> Profil
                         </a>
                     </li>
