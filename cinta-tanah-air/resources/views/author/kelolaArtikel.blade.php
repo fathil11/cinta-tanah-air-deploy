eceffe566e2096f1bc14ccf251c318df86659843
fathil11 <fathil.arham@gmail.com>
2019-08-26 04:08:05 +0700

    Tes

--- cinta-tanah-air/resources/views/author/kelolaArtikel.blade.php
+++ cinta-tanah-air/resources/views/author/kelolaArtikel.blade.php
@@ -21,19 +21,12 @@
             </button>
         </div>
         @elseif (Session::has('error'))
-        <div class="col-md-12">
-            <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                <div>
-                    @foreach ($errors->all() as $error)
-                    <li>
-                        <span class="alert-inner--text">{{ $error }}</span>
-                    </li>
-                    @endforeach
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            </div>
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <span class="alert-inner--icon"><i class="ni ni-bell-55"></i></span>
+            <span class="alert-inner--text"> {{ Session::get('error') }}</span>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
         @endif
     </div>
