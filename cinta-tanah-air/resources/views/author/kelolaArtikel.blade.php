c5717ea4b413edebafba39c03fc44ca8504fec97
fathil11 <fathil.arham@gmail.com>
2019-08-26 01:10:02 +0700

    Add Action Buttin in Author Kelola Artikel

--- cinta-tanah-air/resources/views/author/kelolaArtikel.blade.php
+++ cinta-tanah-air/resources/views/author/kelolaArtikel.blade.php
@@ -118,6 +118,22 @@
                                 </span>
                                 @endif
                             </td>
+                            <td>
+                            <td class="text-right">
+                                <div class="dropdown">
+                                    <a class="btn btn-sm btn-icon-only text-light" href="#" role="button"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fas fa-ellipsis-v"></i>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                        <a class="dropdown-item"
+                                            href="{{ url('author/edit-artikel') . '/' . $article->id }}">Edit</a>
+                                        <a class="dropdown-item"
+                                            href="{{ url('author/delete-artikel') . '/' . $article->id }}">Delete</a>
+                                    </div>
+                                </div>
+                            </td>
+                            </td>
                         </tr>
                         @endforeach
                     </tbody>
