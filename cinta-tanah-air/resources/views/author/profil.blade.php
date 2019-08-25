14f0a21ae13880855b909738e053c2df7022be0f
fathil11 <fathil.arham@gmail.com>
2019-08-26 03:54:39 +0700

    Fix Profil Message Layout

--- cinta-tanah-air/resources/views/author/profil.blade.php
+++ cinta-tanah-air/resources/views/author/profil.blade.php
@@ -11,30 +11,30 @@
 
 @section('content')
 <div class="row">
-    @if (Session::has('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        <span class="alert-inner--icon"><i class="ni ni-bell-55"></i></span>
-        <span class="alert-inner--text"> {{ Session::get('success') }}</span>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    @elseif (Session::has('errors'))
-    <div class="col-md-12">
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <div @foreach ($errors->all() as $error)
-                <li>
-                    <span class="alert-inner--text">{{ $error }}</span>
-                </li>
-                @endforeach
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+    <div class="col">
+        @if (Session::has('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <span class="alert-inner--icon"><i class="ni ni-bell-55"></i></span>
+            <span class="alert-inner--text"> {{ Session::get('success') }}</span>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        @elseif (Session::has('errors'))
+        <div class="col-md-12">
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <div @foreach ($errors->all() as $error)
+                    <li>
+                        <span class="alert-inner--text">{{ $error }}</span>
+                    </li>
+                    @endforeach
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-    @endif
-    <div class="col">
+        @endif
         <div class="card shadow cardcustom">
             <form action="{{ url('author/edit-profil') }}" method="post" enctype="multipart/form-data">
                 {{ csrf_field() }}
