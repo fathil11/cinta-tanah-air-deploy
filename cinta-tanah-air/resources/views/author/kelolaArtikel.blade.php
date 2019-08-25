cae209fd8c4a0a71f25083cd8f100c301d31fa3b
fathil11 <fathil.arham@gmail.com>
2019-08-26 04:17:03 +0700

    Refactoring Message

--- cinta-tanah-air/resources/views/author/kelolaArtikel.blade.php
+++ cinta-tanah-air/resources/views/author/kelolaArtikel.blade.php
@@ -12,23 +12,7 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
-        @if (Session::has('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <span class="alert-inner--icon"><i class="ni ni-bell-55"></i></span>
-            <span class="alert-inner--text"> {{ Session::get('success') }}</span>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        @elseif (Session::has('error'))
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <span class="alert-inner--icon"><i class="ni ni-bell-55"></i></span>
-            <span class="alert-inner--text"> {{ Session::get('error') }}</span>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        @endif
+        @include('layouts.validationMessage')
     </div>
     <div class="col-md-12">
         <div class="card shadow cardcustom">
