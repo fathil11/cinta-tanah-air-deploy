d3b82be0fb1913dcaed54496856a5ee249ee1391
fathil11 <fathil.arham@gmail.com>
2019-08-26 04:01:43 +0700

    Error test

--- cinta-tanah-air/resources/views/author/kelolaArtikel.blade.php
+++ cinta-tanah-air/resources/views/author/kelolaArtikel.blade.php
@@ -20,14 +20,19 @@
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
-        @endif
-        @if (Session::has('error'))
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <span class="alert-inner--icon"><i class="ni ni-bell-55"></i></span>
-            <span class="alert-inner--text"> {{ Session::get('error') }}</span>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
+        @elseif (Session::has('errors'))
+        <div class="col-md-12">
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <div @foreach ($errors->all() as $error)
+                    <li>
+                        <span class="alert-inner--text">{{ $error }}</span>
+                    </li>
+                    @endforeach
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            </div>
         </div>
         @endif
     </div>
