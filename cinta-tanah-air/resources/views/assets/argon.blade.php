99f126dfc91f225bc45271816d0c1c7138ca2f92
fathil11 <fathil.arham@gmail.com>
2019-08-26 07:00:08 +0700

    Testing 1

---
+++ cinta-tanah-air/resources/views/assets/argon.blade.php
@@ -0,0 +1,18 @@
+<link rel="icon" href="{{ asset('img/favicon.png') }}">
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+<!-- Icons -->
+<link href="{{ asset('vendor/nucleo/css/nucleo.css') }}" rel="stylesheet">
+<link href="{{ asset('vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+<!-- Argon CSS -->
+<link type="text/css" href="{{ asset('css/argon.css?v=1.0.0') }}" rel="stylesheet">
+<!-- Custom CSS -->
+<link type="text/css" href="{{ asset('css/admincustom.css') }}" rel="stylesheet">
+<!-- Jordy CSS -->
+<link rel="stylesheet" href="{{ asset('css/jodit.min.css') }}">
+<!-- Custom Jordy CSS -->
+<link rel="stylesheet" type="text/css" href="{{ asset('css/customJordy.css') }}" rel="stylesheet">
+{{-- Data Table CSS--}}
+<link rel="stylesheet" type="text/css" href="{{ asset('css/dataTables.bootstrap4.min.css') }}">
+{{-- Image Crop CSS--}}
+<link rel="stylesheet" href="{{ asset('css/imgareaselect-default.css') }}">
