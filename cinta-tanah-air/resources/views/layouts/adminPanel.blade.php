67fa13002a34fb623f2850b820f6d0a869b7ddf0
fathil11 <fathil.arham@gmail.com>
2019-08-29 18:59:53 +0700

    Tes

--- cinta-tanah-air/resources/views/layouts/adminPanel.blade.php
+++ cinta-tanah-air/resources/views/layouts/adminPanel.blade.php
@@ -473,7 +473,7 @@
                             }
                         },
                         data: {
-                            labels: [stat['h-6'], stat['h-5'], stat['h-4'], stat['h-3'], stat['h-2'], stat['h-1'], 'Hari ini'],
+                            labels: ['1', '2', '3', '4', '5', '6', 'Hari ini'],
                             datasets: [{
                                 label: 'Performance',
                                 data: [stat['h-6_art'], stat['h-5_art'], stat['h-4_art'], stat['h-3_art'], stat['h-2_art'], stat['h-1_art'], stat['today_art']]
