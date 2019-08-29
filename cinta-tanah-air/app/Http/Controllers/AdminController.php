764423a25551144bde5753593727bfd15fbdda82
fathil11 <fathil.arham@gmail.com>
2019-08-29 19:09:23 +0700

    Tes 2

--- cinta-tanah-air/app/Http/Controllers/AdminController.php
+++ cinta-tanah-air/app/Http/Controllers/AdminController.php
@@ -73,7 +73,7 @@ class AdminController extends Controller
         $spec_stat['h-5_art'] = count(Article::whereDate('created_at', Carbon::today()->subDays(5))->get());
         $spec_stat['h-6_art'] = count(Article::whereDate('created_at', Carbon::today()->subDays(6))->get());
         Carbon::setLocale('id');
-        $spec_stat['today'] = Carbonn::today()->format('l');
+        $spec_stat['today'] = Carbon::today()->format('l');
         $spec_stat['h-1'] = Carbon::today()->subDays(1)->format('l');
         $spec_stat['h-2'] = Carbon::today()->subDays(2)->format('l');
         $spec_stat['h-3'] = Carbon::today()->subDays(3)->format('l');
