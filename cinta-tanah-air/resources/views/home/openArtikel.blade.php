824592358922f1545435495c1228f093b86dd808
fathil11 <fathil.arham@gmail.com>
2019-08-29 23:48:01 +0700

    Disable Comment Section in OpenArtikel

--- cinta-tanah-air/resources/views/home/openArtikel.blade.php
+++ cinta-tanah-air/resources/views/home/openArtikel.blade.php
@@ -133,34 +133,34 @@ Cinta Tanah Air
         </div>
     </div>
     </div> --}}
-    <div class="comment-form">
+    {{-- <div class="comment-form">
         <h4>Tulis Komentar</h4>
         <form class="form-contact comment_form" action="{{ url('posting-komentar') . '/' . $article->slug }}"
-            id="commentForm" method="post">
-            @csrf
-            <div class="row">
-                <div class="col-12">
+    id="commentForm" method="post">
+    @csrf
+    <div class="row">
+        <div class="col-12">
 
-                    <div class="form-group">
-                        <textarea class="form-control w-100" name="comment" id="comment" cols="30" rows="3"
-                            placeholder="Tulis komentar ..."></textarea>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="form-group">
-                        <input class="form-control" name="name" id="name" type="text" placeholder="Nama">
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="form-group">
-                        <input class="form-control" name="email" id="email" type="email" placeholder="Email">
-                    </div>
-                </div>
-            </div>
             <div class="form-group">
-                <button type="submit" class="button button-contactForm">Kirim Komentar</button>
+                <textarea class="form-control w-100" name="comment" id="comment" cols="30" rows="3"
+                    placeholder="Tulis komentar ..."></textarea>
             </div>
-        </form>
+        </div>
+        <div class="col-sm-6">
+            <div class="form-group">
+                <input class="form-control" name="name" id="name" type="text" placeholder="Nama">
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="form-group">
+                <input class="form-control" name="email" id="email" type="email" placeholder="Email">
+            </div>
+        </div>
     </div>
+    <div class="form-group">
+        <button type="submit" class="button button-contactForm">Kirim Komentar</button>
+    </div>
+    </form>
+    </div> --}}
     </div>
     @endsection
