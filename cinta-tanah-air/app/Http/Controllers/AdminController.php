6bcf257fd986c47f3e89aa630a1b13cee5ab5e1c
fathil11 <fathil.arham@gmail.com>
2019-08-29 21:30:03 +0700

    Tes 1

--- cinta-tanah-air/app/Http/Controllers/AdminController.php
+++ cinta-tanah-air/app/Http/Controllers/AdminController.php
@@ -99,6 +99,8 @@ class AdminController extends Controller
         $spec_stat['h-4'] = $this->dayTranslator(Carbon::today()->subDays(4)->format('l'));
         $spec_stat['h-6'] = $this->dayTranslator(Carbon::today()->subDays(6)->format('l'));
 
+        $article = Article::with('article_statistic')->withCount('article_statistic')->orderBy('article_id', 'desc')->take(2);
+        dd($article);
         return view('admin.statistik', ['stat' => $stat, 'spec_stat' => $spec_stat]);
     }
 
