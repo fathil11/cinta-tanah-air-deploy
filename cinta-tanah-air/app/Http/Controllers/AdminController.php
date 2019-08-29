e5ff2ecb5f3860e659424999e181da5816e66a0d
fathil11 <fathil.arham@gmail.com>
2019-08-29 21:43:48 +0700

    Fix miss word

--- cinta-tanah-air/app/Http/Controllers/AdminController.php
+++ cinta-tanah-air/app/Http/Controllers/AdminController.php
@@ -99,9 +99,9 @@ class AdminController extends Controller
         $spec_stat['h-4'] = $this->dayTranslator(Carbon::today()->subDays(4)->format('l'));
         $spec_stat['h-6'] = $this->dayTranslator(Carbon::today()->subDays(6)->format('l'));
 
-        $article = Article::with('statistic')->withCount('statistic')->take(5);
+        $articles = Article::with('statistic')->withCount('statistic')->take(5);
 
-        return view('admin.statistik', ['stat' => $stat, 'spec_stat' => $spec_stat, 'article' = $article]);
+        return view('admin.statistik', ['stat' => $stat, 'spec_stat' => $spec_stat, 'articles' => $articles]);
     }
 
     public function showBuatArtikel()
