aa8853d99e6e8c98e63ca1ae2dc448e82d35f1d7
fathil11 <fathil.arham@gmail.com>
2019-10-04 20:21:58 +0700

    Home Testing

--- cinta-tanah-air/app/Http/Controllers/HomeController.php
+++ cinta-tanah-air/app/Http/Controllers/HomeController.php
@@ -78,7 +78,7 @@ class HomeController extends Controller
 
         $stat = $this->countStat();
 
-        return view('home.home', ['stat' => $stat]);
+        return view('home', ['stat' => $stat]);
     }
 
     public function getUserIpAddr(){
