a299bb8d420c057935ef08ce495dd6b4658a72bd
fathil11 <fathil.arham@gmail.com>
2019-10-04 21:56:15 +0700

    Add more news

--- cinta-tanah-air/resources/views/home.blade.php
+++ cinta-tanah-air/resources/views/home.blade.php
@@ -264,132 +264,44 @@
                                     </div>
                                 </div>
                             </div>
-
-                            <!-- Single Featured Post -->
-                            {{-- <div class="single-blog-post featured-post-2">
-                                <div class="post-thumb">
-                                    <a href="#"><img src="img/bg-img/18.jpg" alt=""></a>
-                                </div>
-                                <div class="post-data">
-                                    <a href="#" class="post-catagory">Finance</a>
-                                    <div class="post-meta">
-                                        <a href="#" class="post-title">
-                                            <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus
-                                                sit amet odio sodales placerat. Sed varius leo ac...</h6>
-                                        </a>
-                                        <!-- Post Like & Post Comment -->
-                                        <div class="d-flex align-items-center">
-                                            <a href="#" class="post-like"><img src="img/core-img/like.png" alt="">
-                                                <span>392</span></a>
-                                            <a href="#" class="post-comment"><img src="img/core-img/chat.png" alt="">
-                                                <span>10</span></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> --}}
                         </div>
                     </div>
                 </div>
 
                 <div class="col-12 col-md-6 col-lg-4">
                     <!-- Single Featured Post -->
-                    <div class="single-blog-post small-featured-post d-flex">
+                    @foreach ($articles as $key => $article)
+                    @if ($key > 2 && $key < 7) <div class="single-blog-post small-featured-post d-flex">
                         <div class="post-thumb">
-                            <a href="#"><img src="img/bg-img/19.jpg" alt=""></a>
+                            <a href="{{ url('lihat-artikel') . '/' . $article->slug }}"><img
+                                    src="{{ url('cinta-tanah-air/public/img/blog') . '/' . $article->banner_path }}"
+                                    alt="gambar_artikel"></a>
                         </div>
                         <div class="post-data">
-                            <a href="#" class="post-catagory">Finance</a>
+                            <a href="#" class="post-catagory">
+                                @php
+                                $temp = array();
+                                foreach($article->category as $cats){
+                                $temp[] = $cats->category;
+                                }
+                                $cat = implode(',', $temp)
+                                @endphp
+                                {{ $cat }}
+                            </a>
                             <div class="post-meta">
                                 <a href="#" class="post-title">
-                                    <h6>Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.</h6>
+                                    <h6>{{ $article->title }}</h6>
                                 </a>
-                                <p class="post-date"><span>7:00 AM</span> | <span>April 14</span></p>
+                                <p class="post-date"><span>{{ date('h:i a', strtotime($article->created_at)) }}</span> |
+                                    <span>{{ date('F d', strtotime($article->created_at)) }}</span></p>
                             </div>
                         </div>
-                    </div>
-
-                    <!-- Single Featured Post -->
-                    <div class="single-blog-post small-featured-post d-flex">
-                        <div class="post-thumb">
-                            <a href="#"><img src="img/bg-img/20.jpg" alt=""></a>
-                        </div>
-                        <div class="post-data">
-                            <a href="#" class="post-catagory">Politics</a>
-                            <div class="post-meta">
-                                <a href="#" class="post-title">
-                                    <h6>Sed a elit euismod augue semper congue sit amet ac sapien.</h6>
-                                </a>
-                                <p class="post-date"><span>7:00 AM</span> | <span>April 14</span></p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Single Featured Post -->
-                    <div class="single-blog-post small-featured-post d-flex">
-                        <div class="post-thumb">
-                            <a href="#"><img src="img/bg-img/21.jpg" alt=""></a>
-                        </div>
-                        <div class="post-data">
-                            <a href="#" class="post-catagory">Health</a>
-                            <div class="post-meta">
-                                <a href="#" class="post-title">
-                                    <h6>Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.</h6>
-                                </a>
-                                <p class="post-date"><span>7:00 AM</span> | <span>April 14</span></p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Single Featured Post -->
-                    <div class="single-blog-post small-featured-post d-flex">
-                        <div class="post-thumb">
-                            <a href="#"><img src="img/bg-img/22.jpg" alt=""></a>
-                        </div>
-                        <div class="post-data">
-                            <a href="#" class="post-catagory">Finance</a>
-                            <div class="post-meta">
-                                <a href="#" class="post-title">
-                                    <h6>Augue semper congue sit amet ac sapien. Fusce consequat.</h6>
-                                </a>
-                                <p class="post-date"><span>7:00 AM</span> | <span>April 14</span></p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Single Featured Post -->
-                    <div class="single-blog-post small-featured-post d-flex">
-                        <div class="post-thumb">
-                            <a href="#"><img src="img/bg-img/23.jpg" alt=""></a>
-                        </div>
-                        <div class="post-data">
-                            <a href="#" class="post-catagory">Travel</a>
-                            <div class="post-meta">
-                                <a href="#" class="post-title">
-                                    <h6>Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.</h6>
-                                </a>
-                                <p class="post-date"><span>7:00 AM</span> | <span>April 14</span></p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Single Featured Post -->
-                    <div class="single-blog-post small-featured-post d-flex">
-                        <div class="post-thumb">
-                            <a href="#"><img src="img/bg-img/24.jpg" alt=""></a>
-                        </div>
-                        <div class="post-data">
-                            <a href="#" class="post-catagory">Politics</a>
-                            <div class="post-meta">
-                                <a href="#" class="post-title">
-                                    <h6>Augue semper congue sit amet ac sapien. Fusce consequat.</h6>
-                                </a>
-                                <p class="post-date"><span>7:00 AM</span> | <span>April 14</span></p>
-                            </div>
-                        </div>
-                    </div>
                 </div>
+                @endif
+                @endforeach
             </div>
         </div>
+    </div>
     </div>
     <!-- ##### Featured Post Area End ##### -->
 
