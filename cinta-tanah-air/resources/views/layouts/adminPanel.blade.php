ec2fb8f3cfe0ccb8afa506939827b29f6704be12
fathil11 <fathil.arham@gmail.com>
2019-10-05 12:00:18 +0700

    Admin Upload Image

--- cinta-tanah-air/resources/views/layouts/adminPanel.blade.php
+++ cinta-tanah-air/resources/views/layouts/adminPanel.blade.php
@@ -439,6 +439,15 @@
 
     @if (Request::is('admin/statistik'))
     <script>
+        if($('#editor').length != 0){
+            var editor = new Jodit("#editor", {
+                "uploader": {
+                    "insertImageAsBase64URI": true
+                },
+                "spellcheck": false,
+                "buttons": "paragraph,bold,italic,underline,strikethrough,,,|,fontsize,,brush,|,indent,,align,,ul,ol,|,image,video,table,link,|,undo,redo,\n,cut,hr,eraser,copyformat,|,symbol,fullsize,selectall,print"
+            });
+        }
         // Chart Stat Init
             var ArticleStatistic = (function () {
 
